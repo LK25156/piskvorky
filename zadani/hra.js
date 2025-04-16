700d348c5d23c85b41f2b767c13fb198ae9c300a
Lenka Koubková <lvseteckova@gmail.com>
2025-04-16 20:31:00 +0200

    úprava html,třídy kroužek

--- zadani/hra.js
+++ zadani/hra.js
@@ -1,9 +1,7 @@
 let currentPlayer = "circle"; //proměnná, která uchová info, kdo je na tahu
 
-const fields = document.querySelectorAll(".hraci-pole"); //všechna políčka na herní ploše
-const playerElement = document.querySelector(".prave-hraje"); //element, kdo právě hraje
-//const playerImage = document.querySelector(".prave-hraje img");
-
+const fields = document.querySelectorAll(".hraci_pole"); //všechna políčka na herní ploše
+const playerElement = document.querySelector(".prave_hraje"); //element, kdo právě hraje
 
 for(let i = 0; i < 10;i++) {               //cyklus projde prvních deset políček
 fields[i].addEventListener("click", (event) => {         //posluchač události, až na něj někdo klikne
@@ -11,23 +9,22 @@ fields[i].addEventListener("click", (event) => {         //posluchač události,
 
   if(currentPlayer === "circle") {                //když je na tahu kolečko, přepíšeš do textu nahoru křížek
     field.classList.add ("board_field--circle");
-    //playerElement.src = "cross.svg";
-    playerElement.textContent = "Hraje: křížek";
+    playerElement.textContent = "hraje: křížek";
     currentPlayer = "cross";
   } else {
     field.classList.add ("board_field--cross");  //když je na tahu křížek, přepíšeš do textu nahoru kolečko
-    playerElement.textContent = "Hraje: kolečko";
+    playerElement.textContent = "hraje: kolečko";
     currentPlayer = "circle";
   }
 
   field.disabled = true;              //na toto políčko už znovu nejde kliknout
-                      
 });
 }
 
+
 //Restart hry
 
-const restartLink = document.querySelector (".znovu");
+const restartLink = document.querySelector (".game_link");
 
 restartLink.addEventListener("click", (event) => {
   const confirmed = confirm("Opravdu chceš restartovat hru?");
