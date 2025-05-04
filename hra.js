cfbba179747d8b38e53617633d9c5421faae98bd
Lenka Koubková <lvseteckova@gmail.com>
2025-05-04 16:19:04 +0200

    úpravy css a hra.js

--- hra.js
+++ hra.js
@@ -11,23 +11,36 @@ button.addEventListener("click", (event) => {
   if(currentPlayer === "circle") {                
     button.classList.add ("board_field--circle");
     currentPlayer = "cross";
-    playerElement.innerHTML = `<img src="cross--white.svg">`;
-    //playerElement.textContent = "Hraje: křížek";
-  } else if (currentPlayer === 'cross') {
+    playerElement.innerHTML = `Hraje: <img src="cross--white.svg">`;
+    
+  } else  {
     button.classList.add ("board_field--cross");
-    playerElement.innerHTML =`<img src="circle.svg"> `;
-    //playerElement.textContent = "Hraje: kolečko";
+    playerElement.innerHTML =`Hraje: <img src="circle.svg"> `;
     currentPlayer = "circle";
-  }
-  button.disabled = true;              //na toto políčko už znovu nejde kliknout
-                      
+    }
+  button.disabled = true; //na toto políčko už znovu nejde kliknout                   
 });
 });
+
+//Získání aktuálního stavu herního pole
+const herniPole = Array.from(document.querySelectorAll(".hraci-pole")).map((btn) => {
+  if(btn.classList.contains("board_field--circle")) return"o";
+  if(btn.classList.contains("board_field--cross")) return "x";
+  return"_";
+});
+
+//Kontrola vítěze
+const vitez = findWinner(herniPole);
+if (vitez=== "o" || vitez=== "x" ) {
+  setTimeout(() => {
+    alert(`Vyhrál hráč se symbolem ${vitez}!`);
+    location.reload(); //Restart hry po oznámení vítěze
+  }, 200);
+}
+
 
 //Restart hry
-
 const restartLink = document.querySelector (".znovu");
-
 restartLink.addEventListener("click", (event) => {
   const response = confirm("Opravdu chceš restartovat hru?");
   if (!response) {
