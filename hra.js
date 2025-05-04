1e63748aa0c7a17790845e8d00cec2bf97efdfe4
Lenka Koubková <lvseteckova@gmail.com>
2025-05-04 16:49:56 +0200

    úpravy v hra.js

--- hra.js
+++ hra.js
@@ -9,18 +9,17 @@ let playerElement = document.querySelector(".prave-hraje");
 button.forEach((button) => {
 button.addEventListener("click", (event) => {         
   if(currentPlayer === "circle") {                
-    button.classList.add ("board_field--circle");
+    button.classList.add("board_field--circle");
     currentPlayer = "cross";
     playerElement.innerHTML = `Hraje: <img src="cross--white.svg">`;
     
   } else  {
-    button.classList.add ("board_field--cross");
+    button.classList.add("board_field--cross");
     playerElement.innerHTML =`Hraje: <img src="circle.svg"> `;
     currentPlayer = "circle";
     }
   button.disabled = true; //na toto políčko už znovu nejde kliknout                   
-});
-});
+
 
 //Získání aktuálního stavu herního pole
 const herniPole = Array.from(document.querySelectorAll(".hraci-pole")).map((btn) => {
@@ -33,11 +32,12 @@ const herniPole = Array.from(document.querySelectorAll(".hraci-pole")).map((btn)
 const vitez = findWinner(herniPole);
 if (vitez=== "o" || vitez=== "x" ) {
   setTimeout(() => {
-    alert(`Vyhrál hráč se symbolem ${vitez}!`);
+    alert(`Vyhrál hráč se symbolem ${vitez}.`)
     location.reload(); //Restart hry po oznámení vítěze
   }, 200);
 }
-
+});
+});
 
 //Restart hry
 const restartLink = document.querySelector (".znovu");
